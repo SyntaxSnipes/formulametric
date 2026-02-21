b0d290634125a80d40c8eb769a601134b0043c41
SyntaxCodes <mohammedaayan@icloud.com>
2026-02-21 19:48:00 +0400

    added extra componenent for navbar

--- fm/src/App.jsx
+++ fm/src/App.jsx
@@ -1,26 +1,10 @@
-import {  } from 'react'
-import fmlogo from '/fmicon.png'
 import './styles/index.css'
 import { Route, Routes } from 'react-router-dom'
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
 import Home from './home'
 import Drivers from './drivers'
 import PropTypes from 'prop-types';
-
-function NavBar() {
-
-  return (
-    <nav>
-      <div className="flex justify-between items-center px-8 py-[0.3rem] h-[4.3rem] bg-[#15151e] text-[#ff1e00] fixed top-[0] left-[0] w-screen">
-        <img className='w-[4.2rem] h-auto' src={fmlogo} alt="Formula Metric" />
-        <span className='flex gap-4 list-none'>
-          <CustomLink className='[all:unset] cursor-pointer text-[1.5rem]' to="/">Home</CustomLink>
-          <CustomLink className='[all:unset] cursor-pointer text-[1.5rem]' to="/drivers">Drivers</CustomLink>
-        </span>
-      </div>
-    </nav>
-  )
-}
+import NavBar from './components/navbar'
 
 function App() {
   return (
---
+++ fm/src/components/navbar.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import fmlogo from '/fmicon.png'
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
+export default function NavBar() {
+  return (
+    <nav>
+      <div className="flex justify-between items-center px-8 py-[0.3rem] h-[4.3rem] bg-[#15151e] text-[#ff1e00] fixed top-[0] left-[0] w-screen">
+        <img className="w-[4.2rem] h-auto" src={fmlogo} alt="Formula Metric" />
+        <span className="flex gap-4 list-none">
+          <CustomLink
+            className="[all:unset] cursor-pointer text-[1.5rem]"
+            to="/"
+          >
+            Home
+          </CustomLink>
+          <CustomLink
+            className="[all:unset] cursor-pointer text-[1.5rem]"
+            to="/drivers"
+          >
+            Drivers
+          </CustomLink>
+        </span>
+      </div>
+    </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
+
+CustomLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired 
+};
